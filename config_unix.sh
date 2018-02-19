449dc38f06a931d58c5142e8ca68fb7513aa10f1
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-02-19 18:14:20 +0100

    Add global gitignore and add clang_complete to it

--- config_unix.sh
+++ config_unix.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ln -sf "$(pwd)/.gitconfig" ~/.gitconfig
+ln -sf "$(pwd)/.gitignore_global" ~/.gitignore_global
 ln -sf "$(pwd)/.latexmkrc" ~/.latexmkrc
 
--- config_win.ps1
+++ config_win.ps1
@@ -1,3 +1,4 @@
 cmd /c mklink /H %USERPROFILE%\.gitconfig .gitconfig
+cmd /c mklink /H %USERPROFILE%\.gitignore_global .gitignore_global
 cmd /c mklink /H %USERPROFILE%\.latexmkrc .latexmkrc
 cmd /c mklink /H %USERPROFILE%\.minttyrc .minttyrc
