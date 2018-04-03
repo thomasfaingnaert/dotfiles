53c663a7b386a03f51000d7e6522cbdabde3fab5
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-03 12:29:55 +0200

    Add global YouCompleteMe configuration

--- config_unix.sh
+++ config_unix.sh
@@ -2,4 +2,4 @@
 ln -sf "$(pwd)/.gitconfig" ~/.gitconfig
 ln -sf "$(pwd)/.gitignore_global" ~/.gitignore_global
 ln -sf "$(pwd)/.latexmkrc" ~/.latexmkrc
-
+ln -sf "$(pwd)/.ycm_extra_conf_global.py" ~/.ycm_extra_conf_global.py
