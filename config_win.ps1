a1bcf180bcb9278d5fc6a188bf0c1f336d0a812f
unknown <thomas.faingnaert@hotmail.com>
2018-04-04 18:59:21 +0200

    Revert "Add global YouCompleteMe configuration"
    This reverts commit 53c663a7b386a03f51000d7e6522cbdabde3fab5.

--- config_win.ps1
+++ config_win.ps1
@@ -2,4 +2,3 @@ cmd /c mklink /H %USERPROFILE%\.gitconfig .gitconfig
 cmd /c mklink /H %USERPROFILE%\.gitignore_global .gitignore_global
 cmd /c mklink /H %USERPROFILE%\.latexmkrc .latexmkrc
 cmd /c mklink /H %USERPROFILE%\.minttyrc .minttyrc
-cmd /c mklink /H %USERPROFILE%\.ycm_extra_conf_global.py .ycm_extra_conf_global.py
