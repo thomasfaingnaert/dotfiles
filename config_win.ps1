1f50751ca7cbb2069801c4807741a727b82fa016
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-07 01:43:52 +0200

    Remove GUI configuration for NeoVim

--- config_win.ps1
+++ config_win.ps1
@@ -4,7 +4,6 @@ cmd /c mklink /H %USERPROFILE%\.latexmkrc .latexmkrc
 cmd /c mklink /H %USERPROFILE%\.minttyrc .minttyrc
 md $env:LocalAppData\nvim
 cmd /c mklink /H %LOCALAPPDATA%\nvim\init.vim nvim\init.vim
-cmd /c mklink /H %LOCALAPPDATA%\nvim\ginit.vim nvim\ginit.vim
 md $env:LocalAppData\nvim\autoload
 $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 (New-Object Net.WebClient).DownloadFile(
--- nvim/init.vim
+++ nvim/init.vim
@@ -1,5 +1,3 @@
 call plug#begin()
 
-Plug 'lifepillar/vim-solarized8'
-
 call plug#end()
