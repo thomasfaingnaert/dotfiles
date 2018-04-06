9a9605f329c38dfee5b632445ba3c6a3f9d0526b
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-07 00:43:10 +0200

    Add vim-plug

--- config_win.ps1
+++ config_win.ps1
@@ -2,5 +2,11 @@ cmd /c mklink /H %USERPROFILE%\.gitconfig .gitconfig
 cmd /c mklink /H %USERPROFILE%\.gitignore_global .gitignore_global
 cmd /c mklink /H %USERPROFILE%\.latexmkrc .latexmkrc
 cmd /c mklink /H %USERPROFILE%\.minttyrc .minttyrc
-mkdir $env:LocalAppData\nvim
+md $env:LocalAppData\nvim
 cmd /c mklink /H %LOCALAPPDATA%\nvim\init.vim nvim\init.vim
+md $env:LocalAppData\nvim\autoload
+$uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+(New-Object Net.WebClient).DownloadFile(
+    $uri,
+    $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$env:LocalAppData\nvim\autoload\plug.vim")
+)
