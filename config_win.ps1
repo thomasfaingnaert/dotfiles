d3e3aebfae98e1d0251c929b8a0538b9d6a6e16d
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-07 00:28:12 +0200

    Add empty Neovim configuration

--- config_win.ps1
+++ config_win.ps1
@@ -2,3 +2,5 @@ cmd /c mklink /H %USERPROFILE%\.gitconfig .gitconfig
 cmd /c mklink /H %USERPROFILE%\.gitignore_global .gitignore_global
 cmd /c mklink /H %USERPROFILE%\.latexmkrc .latexmkrc
 cmd /c mklink /H %USERPROFILE%\.minttyrc .minttyrc
+mkdir $env:LocalAppData\nvim
+cmd /c mklink /H %LOCALAPPDATA%\nvim\init.vim nvim\init.vim
