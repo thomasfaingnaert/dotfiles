0d03037aaa077ca3a7892dec4ad6e56efa5f373f
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-07 20:01:40 +0200

    Move Vim config to dotfiles repo

--- config_win.ps1
+++ config_win.ps1
@@ -2,15 +2,17 @@ cmd /c mklink /H %USERPROFILE%\.gitconfig .gitconfig
 cmd /c mklink /H %USERPROFILE%\.gitignore_global .gitignore_global
 cmd /c mklink /H %USERPROFILE%\.latexmkrc .latexmkrc
 cmd /c mklink /H %USERPROFILE%\.minttyrc .minttyrc
-md $env:LocalAppData\nvim
-cmd /c mklink /H %LOCALAPPDATA%\nvim\init.vim nvim\init.vim
-cmd /c mklink /H %LOCALAPPDATA%\nvim\ginit.vim nvim\ginit.vim
-md $env:LocalAppData\nvim\autoload
+
+cmd /c mklink /J %USERPROFILE%\vimfiles vim
+
+md -Force ~\vimfiles\autoload
+md -Force ~\vimfiles\backup
+md -Force ~\vimfiles\swap
+md -Force ~\vimfiles\undo
 $uri = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 (New-Object Net.WebClient).DownloadFile(
-    $uri,
-    $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("$env:LocalAppData\nvim\autoload\plug.vim")
+  $uri,
+  $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath(
+    "~\vimfiles\autoload\plug.vim"
+  )
 )
-nvim +PlugInstall +qall
-md $env:AppData\Oni
-cmd /c mklink /H %APPDATA%\Oni\config.tsx oni\config.tsx
