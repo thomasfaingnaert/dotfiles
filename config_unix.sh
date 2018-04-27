4fa893343be27deb4c07b7b5a1e08ed726d06eb6
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-04-27 20:48:23 +0200

    Add vim config to config_unix.sh

--- config_unix.sh
+++ config_unix.sh
@@ -3,3 +3,14 @@ ln -sf "$(pwd)/.gitconfig" ~/.gitconfig
 ln -sf "$(pwd)/.gitignore_global" ~/.gitignore_global
 ln -sf "$(pwd)/.latexmkrc" ~/.latexmkrc
 
+ln -sf "$(pwd)/vim" ~/.vim
+
+mkdir ~/.vim/autoload
+mkdir ~/.vim/backup
+mkdir ~/.vim/swap
+mkdir ~/.vim/undo
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim +PlugInstall +qall
