0c4699a894a87637190ff600fbb3e4aa89f49475
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2018-09-30 17:06:25 +0200

    Add solarized light color scheme to PuTTY

--- config_win.ps1
+++ config_win.ps1
@@ -28,3 +28,33 @@ foreach ($file in $files)
         $f.Attributes = $f.Attributes -bor "Hidden"
     }
 }
+
+if (Test-Path 'HKCU:\Software\SimonTatham\PuTTY\Sessions')
+{
+    Get-ChildItem -Path 'HKCU:\Software\SimonTatham\PuTTY\Sessions' |
+        ForEach-Object {
+            Set-ItemProperty -Path $_.PSPath -Name "Colour0" -Value "101,123,131";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour0" -Value "101,123,131";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour1" -Value "88,110,117";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour2" -Value "253,246,227";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour3" -Value "238,232,213";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour4" -Value "238,232,213";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour5" -Value "101,123,131";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour6" -Value "7,54,66";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour7" -Value "0,43,54";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour8" -Value "220,50,47";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour9" -Value "203,75,22";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour10" -Value "133,153,0";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour11" -Value "88,110,117";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour12" -Value "181,137,0";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour13" -Value "101,123,131";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour14" -Value "38,139,210";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour15" -Value "131,148,150";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour16" -Value "211,54,130";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour17" -Value "108,113,196";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour18" -Value "42,161,152";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour19" -Value "147,161,161";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour20" -Value "238,232,213";
+            Set-ItemProperty -Path $_.PSPath -Name "Colour21" -Value "253,246,227"
+        }
+}
