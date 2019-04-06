9a8f7383f40224b9db49738262439183174e6224
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-04-06 15:52:02 +0200

    Add Yaru install to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,12 @@ install_dotfiles()
     ./config_unix.sh
 }
 
+install_yaru()
+{
+    sudo snap install communitheme
+    sudo update-alternatives --install /usr/share/gnome-shell/theme/gdm3.css gdm3.css /snap/communitheme/current/share/gnome-shell/theme/Communitheme/gnome-shell.css 15
+    sudo sed -ie '/^\[User\]$/,/^\[/ s/^\(XSession=\).*$/\1ubuntu-communitheme-snap/' /var/lib/AccountsService/users/${USER}
+}
+
 install_dotfiles
+install_yaru
