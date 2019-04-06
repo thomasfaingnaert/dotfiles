fc2aef5b522a57b8bd6eb1255e6bc87f9f2f693e
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-04-06 16:30:28 +0200

    Add selection menu to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-install_dotfiles()
+basic_apps_and_dotfiles()
 {
     sudo apt-get install -y vim vim-gnome curl
+    sudo snap install skype --classic
     ./config_unix.sh
+    gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'gvim.desktop', 'skype_skypeforlinux.desktop']"
 }
 
 install_yaru()
@@ -18,18 +20,25 @@ configure_dualboot()
     timedatectl set-local-rtc 1
 }
 
-install_skype()
-{
-    sudo snap install skype --classic
-}
+features=$(
+    whiptail --title "Select Features" --checklist --notags --separate-output \
+    "Choose the features to install:" 10 60 3 \
+    basic       "Basic applications and dotfiles" ON \
+    yaru        "Yaru theme for Ubuntu" ON \
+    dualboot    "Dual boot specific configuration with Windows" ON \
+    3>&1 1>&2 2>&3)
 
-configure_favourites()
-{
-    gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Nautilus.desktop', 'gvim.desktop', 'skype_skypeforlinux.desktop']"
-}
-
-install_dotfiles
-install_yaru
-configure_dualboot
-install_skype
-configure_favourites
+for feature in $features
+do
+    case $feature in
+        "basic")
+            basic_apps_and_dotfiles
+            ;;
+        "yaru")
+            install_yaru
+            ;;
+        "dualboot")
+            configure_dualboot
+            ;;
+    esac
+done
