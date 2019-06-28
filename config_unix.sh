28bd7d94bcaef29cfb8bce697c87f4de4b4c24e5
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-06-28 23:40:05 +0200

    Don't print progress of curl

--- config_unix.sh
+++ config_unix.sh
@@ -10,7 +10,7 @@ mkdir -p ~/.vim/autoload \
          ~/.vim/swap \
          ~/.vim/undo
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+curl -fLsSo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 if command -v vim >/dev/null 2>&1; then
