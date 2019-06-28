af8833e3d926acad86fe9d2817d55a7b9e022116
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-06-28 22:30:42 +0200

    Clean up Yaru feature

--- bootstrap.sh
+++ bootstrap.sh
@@ -80,7 +80,7 @@ feature_yaru()
     sudo update-alternatives --install /usr/share/gnome-shell/theme/gdm3.css gdm3.css /snap/communitheme/current/share/gnome-shell/theme/Communitheme/gnome-shell.css 15
 
     # Use Yaru as default user session for the current user
-    sudo sed -ie '/^\[User\]$/,/^\[/ s/^\(XSession=\).*$/\1ubuntu-communitheme-snap/' /var/lib/AccountsService/users/${USER}
+    sudo sed -i '/^\[User\]$/,/^\[/ s/^\(XSession=\).*$/\1ubuntu-communitheme-snap/' /var/lib/AccountsService/users/${USER}
 
     # Change cursor theme to DMZ-White
     gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-White'
