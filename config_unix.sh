9d7911fcffb3d6ee9737dd8b067ff84b60926858
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-07-03 18:58:43 +0200

    Use Vim configuration for Neovim

--- config_unix.sh
+++ config_unix.sh
@@ -4,6 +4,7 @@ ln -sf "$(pwd)/.gitconfig" ~/.gitconfig
 ln -sf "$(pwd)/.gitignore_global" ~/.gitignore_global
 
 ln -sfn "$(pwd)/vim" ~/.vim
+ln -sfn "$(pwd)/nvim" ~/.config/nvim
 
 mkdir -p ~/.vim/autoload \
          ~/.vim/backup \
---
+++ nvim/ginit.vim
@@ -0,0 +1,8 @@
+set background=light
+colorscheme NeoSolarizedCustom
+
+if has('win32')
+    set guifont=Consolas:h11
+else
+    set guifont=Monospace
+endif
