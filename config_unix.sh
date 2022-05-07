5a4c4e64da2b0c40795a66d2e82cf33233e04511
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2022-05-07 13:49:41 +0200

    Run PlugInstall in headless Neovim

--- config_unix.sh
+++ config_unix.sh
@@ -17,7 +17,7 @@ curl -fLsSo ~/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 if command -v nvim >/dev/null 2>&1; then
-    nvim +PlugInstall +qall
+    nvim --headless +PlugInstall +qall
 elif command -v vim >/dev/null 2>&1; then
     vim +PlugInstall +qall
 else
