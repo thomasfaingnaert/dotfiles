1f12d4590f80f09c17691d85d9d1bb7452aca4e2
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2022-09-02 18:21:27 +0200

    Do not use OneDrive service

--- bootstrap.sh
+++ bootstrap.sh
@@ -406,12 +406,6 @@ feature_zoom()
 feature_onedrive()
 {
     sudo apt-get install -y onedrive
-
-    sudo systemctl stop onedrive@${USER}.service
-    sudo systemctl disable onedrive@${USER}.service
-
-    systemctl --user enable onedrive
-    systemctl --user start onedrive
 }
 
 ########
