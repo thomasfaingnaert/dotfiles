452d819ba12bb379f63b30f8344f2a85b56820f4
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2024-09-24 11:13:15 +0200

    Fix Pomodoro hook not working on stop

--- pomodoro-hook.sh
+++ pomodoro-hook.sh
@@ -7,10 +7,23 @@ set -Eeuo pipefail
 
 # Inspired by: https://ankursinha.in/2017/12/25/managing-tasks-time-and-making-sure-one-takes-a-break-integrating-taskwarrior-timewarrior-and-gnome-pomodoro.html
 
+echo "$(date) $@" >>/tmp/output.log
+
 if [[ "$1" == "start" || "$1" == "resume" ]]; then
-    timew || timew continue
+    if ! timew; then
+        timew continue
+    fi
 fi
 
 if [[ "$1" == "complete" || "$1" == "skip" || "$1" == "pause" ]]; then
-    timew && timew stop
+    if timew; then
+        # When stopping, Pomodoro issues a resume followed immediately by a skip.
+        # Timewarrior throws a "The end of a date range must be after the start."
+        # error, so keep retrying until stopping succeeds. (Up to a maximum of
+        # 60 seconds.)
+        for i in {1..60}; do
+            timew stop && break
+            sleep 1
+        done
+    fi
 fi
