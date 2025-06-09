5c53facc202ab4974b4a62f6a34e643ddc13101b
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2025-06-10 01:20:46 +0200

    Finish

--- archinstall.sh
+++ archinstall.sh
@@ -72,6 +72,8 @@ select PART_SETUP in premounted ext4; do
 
             SWAP_SIZE=$(awk '/MemTotal/ { print $2 * 2 }' /proc/meminfo)
 
+            ESP_PART_NR="1"
+
             ESP_PART="$DISK-part1"
             SWAP_PART="$DISK-part2"
             ROOT_PART="$DISK-part3"
@@ -107,6 +109,8 @@ select PART_SETUP in premounted ext4; do
             mountpoint -q /mnt || die "Nothing mounted on /mnt"
             debug_on
 
+            # TODO: Need $DISK, $ROOT_PART and $ESP_PART_NR
+
             break
             ;;
     esac
@@ -119,3 +123,42 @@ pacstrap -K /mnt base linux linux-firmware
 
 # (3.1) fstab
 genfstab -U /mnt >> /mnt/etc/fstab
+
+# (3.3) Timezone
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Brussels /etc/localtime
+arch-chroot /mnt hwclock --systohc
+
+# TODO: NTP (see wiki 3.3)
+
+# (3.4) Localization
+sed -i 's/#\(en_GB.UTF-8\)/\1/' /mnt/etc/locale.gen
+arch-chroot /mnt locale-gen
+echo 'LANG=en_GB.UTF-8' >/mnt/etc/locale.conf
+
+# (3.5) Network configuration
+read -p "Enter hostname: " HOSTNAME
+echo "$HOSTNAME" >/mnt/etc/hostname
+
+# TODO: Install network software.
+
+# (3.7) Set the root password
+# TODO: Get rid of this?
+arch-chroot /mnt passwd
+
+# (3.8) Boot loader (systemd-boot)
+# TODO: instead of ROOT_PART, use mountpoint.
+# TODO: same with ESP_PART_NR?
+# TODO: same with DISK?
+arch-chroot /mnt bootctl install
+ROOT_UUID=$(blkid "$ROOT_PART" --match-tag UUID --output value)
+efibootmgr --create --disk $DISK --part $ESP_PART_NR --loader '\EFI\systemd\systemd-bootx64.efi' --label "Linux Boot Manager" --unicode
+cat <<EOF >/mnt/boot/loader/entries/arch.conf
+title  Arch Linux
+linux  /vmlinuz-linux
+initrd /initramfs-linux.img
+options root=UUID=$ROOT_UUID rw
+EOF
+
+# (5) Post-installation.
+
+# TODO: See general recommendations (user account, GUI, sound, trackpad).
