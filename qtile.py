3f154d4bc91113af16390be8d956d4e22af8198b
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2025-06-14 14:48:56 +0200

    Configure touchpad for Wayland

--- qtile.py
+++ qtile.py
@@ -2,6 +2,7 @@ from libqtile import bar, layout, qtile, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
+from libqtile.backend.wayland import InputConfig
 
 mod = "mod4"
 terminal = guess_terminal()
@@ -184,7 +185,9 @@ reconfigure_screens = True
 auto_minimize = True
 
 # When using the Wayland backend, this can be used to configure input devices.
-wl_input_rules = None
+wl_input_rules = {
+    'type:touchpad': InputConfig(tap=True, natural_scroll=True)
+}
 
 # xcursor theme (string or None) and size (integer) for Wayland backend
 wl_xcursor_theme = None
