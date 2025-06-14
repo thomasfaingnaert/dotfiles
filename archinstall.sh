9566fdef7220a0ab0f6c7cb96f5532f0ae38a01f
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2025-06-14 12:15:15 +0200

    Install GUI, audio, and bluetooth

--- archinstall.sh
+++ archinstall.sh
@@ -172,9 +172,40 @@ arch-chroot /mnt passwd thomas
 arch-chroot /mnt pacman --noconfirm -S sudo
 sed -i '/# %wheel ALL=(ALL:ALL) ALL/s/^# //' /mnt/etc/sudoers
 
-# TODO: general recommendations: GUI
+# 3.1) Install Qtile (Wayland) as GUI.
+arch-chroot /mnt pacman --noconfirm -S \
+    qtile \
+    libinput \
+    python-pywayland \
+    python-pywlroots \
+    python-xkbcommon \
+    xorg-xwayland
 
-# TODO: general recommendations: sound
+# Autostart qtile on VT1.
+cat >>/home/thomas/.bash_profile <<EOF
+
+if [ -z "\$WAYLAND_DISPLAY" ] && [ -n "\$XDG_VTNR" ] && [ "\$XDG_VTNR" -eq 1 ]; then
+    exec qtile start -b wayland
+fi
+EOF
+
+# 3.2) Install some fonts, as installing Qtile by default doesn't pull in any.
+# NOTE: ttf-noto-nerd is required by bluetui.
+arch-chroot /mnt pacman --noconfirm -S noto-fonts ttf-noto-nerd
+
+# 3.3) Install a terminal.
+arch-chroot /mnt pacman --noconfirm -S foot
+
+# 3.4) Create user directories automatically.
+arch-chroot /mnt pacman --noconfirm -S xdg-user-dirs
+
+# 3.5) Install sounds packages (pipewire).
+arch-chroot /mnt pacman --noconfirm -S pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber
+arch-chroot /mnt pacman --noconfirm -S pavucontrol
+
+# 3.6) Install bluetooth packages (bluez).
+arch-chroot /mnt pacman --noconfirm -S bluez bluez-utils bluetui
+arch-chroot /mnt systemctl enable bluetooth
 
 # TODO: general recommendations: trackpad
 
