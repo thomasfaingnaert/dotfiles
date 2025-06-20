be49e64309b654791bf7f410f3ffee535de9edd4
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2025-06-20 21:38:55 +0200

    Add option to install GNOME

--- scripts/arch-post-install.sh
+++ scripts/arch-post-install.sh
@@ -10,49 +10,64 @@ cd ..
 # TODO: encrypted WiFi passwords (see https://wiki.archlinux.org/title/NetworkManager)
 # TODO: other networkmanager config (see https://wiki.archlinux.org/title/NetworkManager)
 
+# Select between Qtile and Gnome.
 
-# 3.1) Install Qtile (Wayland) as GUI.
-sudo pacman --noconfirm -S \
-    qtile \
-    libinput \
-    python-pywayland \
-    python-pywlroots \
-    python-xkbcommon \
-    xorg-xwayland
+PS3="Which DE/WM do you want to install? "
 
-# Autostart qtile on VT1.
-# NOTE: Our bash_profile from our dotfiles will source profile.local.
-cat >~/.profile.local <<EOF
-if [ -z "\$WAYLAND_DISPLAY" ] && [ -n "\$XDG_VTNR" ] && [ "\$XDG_VTNR" -eq 1 ]; then
-    exec qtile start -b wayland
-fi
+select WM in qtile gnome; do
+    case "$WM" in
+        qtile)
+            # 3.1) Install Qtile (Wayland) as GUI.
+            sudo pacman --noconfirm -S \
+                qtile \
+                libinput \
+                python-pywayland \
+                python-pywlroots \
+                python-xkbcommon \
+                xorg-xwayland
+
+            # Autostart qtile on VT1.
+            # NOTE: Our bash_profile from our dotfiles will source profile.local.
+            cat >~/.profile.local <<EOF
+            if [ -z "\$WAYLAND_DISPLAY" ] && [ -n "\$XDG_VTNR" ] && [ "\$XDG_VTNR" -eq 1 ]; then
+                exec qtile start -b wayland
+            fi
 EOF
 
-# 3.2) Install some fonts, as installing Qtile by default doesn't pull in any.
-# NOTE: ttf-noto-nerd is required by bluetui.
-sudo pacman --noconfirm -S noto-fonts ttf-noto-nerd
+            # 3.2) Install some fonts, as installing Qtile by default doesn't pull in any.
+            # NOTE: ttf-noto-nerd is required by bluetui.
+            sudo pacman --noconfirm -S noto-fonts ttf-noto-nerd
 
-# 3.3) Install a terminal.
-sudo pacman --noconfirm -S foot
+            # 3.3) Install a terminal.
+            sudo pacman --noconfirm -S foot
 
-# 3.4) Create user directories automatically.
-sudo pacman --noconfirm -S xdg-user-dirs
+            # 3.4) Create user directories automatically.
+            sudo pacman --noconfirm -S xdg-user-dirs
 
-# 3.5) Install sounds packages (pipewire).
-sudo pacman --noconfirm -S pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber
-sudo pacman --noconfirm -S pavucontrol
+            # 3.5) Install sounds packages (pipewire).
+            sudo pacman --noconfirm -S pipewire pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack wireplumber
+            sudo pacman --noconfirm -S pavucontrol
 
-# 3.6) Install bluetooth packages (bluez).
-sudo pacman --noconfirm -S bluez bluez-utils bluetui
-sudo systemctl enable bluetooth
+            # 3.6) Install bluetooth packages (bluez).
+            sudo pacman --noconfirm -S bluez bluez-utils bluetui
+            sudo systemctl enable bluetooth
 
-# TODO: general recommendations: trackpad
+            break
+            ;;
 
-# TODO: Secure boot.
+        gnome)
+            # 3.1) Install GNOME
+            sudo pacman --noconfirm -S gnome
+
+            # Autostart GDM on boot.
+            sudo systemctl enable gdm
+
+            break
+            ;;
+    esac
+done
 
 # TODO: Encryption. (+ encrypted swap, hibernation)
-
-# TODO: btrfs?
 
 # TODO: zfs?
 
