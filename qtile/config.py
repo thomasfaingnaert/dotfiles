64cb05fa5add3b07b30d326d5ecffbd2374acd37
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2025-06-22 00:35:41 +0200

    Make pavucontrol floating

--- qtile/config.py
+++ qtile/config.py
@@ -291,6 +291,8 @@ cursor_warp = False
 floating_layout = layout.Floating(
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
+        # Wayland: qtile shell
+        #          > windows()
         *layout.Floating.default_float_rules,
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
@@ -299,6 +301,7 @@ floating_layout = layout.Floating(
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
         Match(title="floatingterm"), # Floating terminal
+        Match(wm_class="org.pulseaudio.pavucontrol"), # PulseAudio Volume Control
     ]
 )
 auto_fullscreen = True
